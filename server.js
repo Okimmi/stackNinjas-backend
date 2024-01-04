b99cd61456b4103b1719cabec7715ab66ab6d5a6
SSSerg05 <ssserg05@gmail.com>
2024-01-04 10:34:21 +0200

    fix sign-up

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const app = require('./app');
-const TelegramBot = require('node-telegram-bot-api');
-const cron = require('node-cron');
-const sendTelegramNotification = require('./utils/sendTelegramNotification');
-const { User } = require('./models/user');
+// const TelegramBot = require('node-telegram-bot-api');
+// const cron = require('node-cron');
+// const sendTelegramNotification = require('./utils/sendTelegramNotification');
+// const { User } = require('./models/user');
 
-const { DB_HOST, PORT, TELEGRAM_BOT_TOKEN } = process.env;
+const { DB_HOST, PORT, } = process.env;
 
-const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
+// const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 
 mongoose
   .connect(DB_HOST)
@@ -22,31 +22,31 @@ mongoose
     process.exit(1);
   });
 
-bot.onText(/\/start (.+)/, async (msg, match) => {
-  const userId = msg.from.id;
-  const commandParam = match[1];
+// bot.onText(/\/start (.+)/, async (msg, match) => {
+//   const userId = msg.from.id;
+//   const commandParam = match[1];
 
-  await User.findByIdAndUpdate(commandParam, { chatId: userId });
-  console.log(`User ${commandParam} started bot`);
-});
+//   await User.findByIdAndUpdate(commandParam, { chatId: userId });
+//   console.log(`User ${commandParam} started bot`);
+// });
 
-bot.onText(/\/stop/, async (msg) => {
-  const chatId = msg.chat.id;
+// bot.onText(/\/stop/, async (msg) => {
+//   const chatId = msg.chat.id;
 
-  const user = await User.findOneAndUpdate({ chatId }, { chatId: null });
+//   const user = await User.findOneAndUpdate({ chatId }, { chatId: null });
 
-  if (user) {
-    console.log(`User ${user._id} stopped bot`);
-  }
-});
+//   if (user) {
+//     console.log(`User ${user._id} stopped bot`);
+//   }
+// });
 
-cron.schedule(
-  '0 19 * * *',
-  () => {
-    sendTelegramNotification(bot);
-  },
-  {
-    scheduled: true,
-    timezone: 'Europe/Kiev',
-  }
-);
+// cron.schedule(
+//   '0 19 * * *',
+//   // () => {
+//   //   sendTelegramNotification(bot);
+//   // },
+//   {
+//     scheduled: true,
+//     timezone: 'Europe/Kiev',
+//   }
+// );
